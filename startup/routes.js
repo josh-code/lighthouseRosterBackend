226a5a6da71f2e409017f4f802258731b376629d
josh-code <fernandes.developer@gmail.com>
2019-10-11 17:26:06 +0530

    frontend url cors

--- startup/routes.js
+++ startup/routes.js
@@ -5,7 +5,7 @@ const user = require("../routes/users");
 const auth = require("../routes/auth");
 const cors = require("cors");
 module.exports = function(app) {
-  app.use(cors({ origin: "http://localhost:3000" }));
+  app.use(cors({ origin: "https://lighthouseroster.herokuapp.com" }));
   app.use(express.json());
   app.use("/api/roster", roster);
   app.use("/api/user", user);
