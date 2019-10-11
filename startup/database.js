1795e5f1f94d9d8b4e4b12750451dc3d560ce7b6
josh-code <fernandes.developer@gmail.com>
2019-10-11 13:54:21 +0530

    third

--- startup/database.js
+++ startup/database.js
@@ -5,7 +5,7 @@ const config = require("config");
 const db = config.get("db");
 module.exports = function() {
   mongoose.set("useCreateIndex", true);
-  mongoose.set("useNewUrlParser", true);
+  // mongoose.set("useNewUrlParser", true);
 
   mongoose.connect(db).then(() => winston.info(`connected to ${db}`));
 };
