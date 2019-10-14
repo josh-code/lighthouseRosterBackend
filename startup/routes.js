26bf28e8cfd10829f3960831192332240b528b46
josh-code <fernandes.developer@gmail.com>
2019-10-14 18:34:51 +0530

    rostering process update, accept roster update

--- startup/routes.js
+++ startup/routes.js
@@ -5,8 +5,8 @@ const user = require("../routes/users");
 const auth = require("../routes/auth");
 const cors = require("cors");
 module.exports = function(app) {
-  // app.use(cors({ origin: "https://lighthouseroster.herokuapp.com" }));
-  app.use(cors({ origin: "http://localhost:3000" }));
+  app.use(cors({ origin: "https://lighthouseroster.herokuapp.com" }));
+  // app.use(cors({ origin: "http://localhost:3000" }));
   app.use(express.json());
   app.use("/api/roster", roster);
   app.use("/api/user", user);
